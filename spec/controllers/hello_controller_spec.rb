96115ca9f6eb33b443e35619be869020913424d2
Yuuki Noguchi <otonakaoru@gmail.com>
2014-07-15 21:40:06 +0900

    [ひと通り実装して処理の流れを確認する]動作確認用のspecを作成しました

--- spec/controllers/hello_controller_spec.rb
+++ spec/controllers/hello_controller_spec.rb
@@ -3,10 +3,18 @@ require 'rails_helper'
 RSpec.describe HelloController, :type => :controller do
 
   describe "GET 'index'" do
-    it "returns http success" do
+    it '200 OK' do
       get 'index'
       expect(response).to be_success
+      expect(response).to be_success
     end
+
+    it 'get hello' do
+      get 'index'
+      json = JSON.parse(response.body)
+      expect(json['hello']).to eq('world')
+    end
+
   end
 
 end
