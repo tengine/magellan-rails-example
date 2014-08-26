36275e2020dbc0ed8bcd0cd338de754913e9a4f8
Shinichirow KAMITO <updoor@gmail.com>
2014-08-26 18:01:06 +0900

    データ確認用にreqsコントローラを作成しました。 すべてのHTTPメソッドを対象として、リクエストされた際のクエリなどの情報を返すようにしました。

---
+++ app/controllers/reqs_controller.rb
@@ -0,0 +1,45 @@
+class ReqsController < ApplicationController
+
+  respond_to :json
+
+  # GET: /reqs
+  def index
+    show_data()
+  end
+
+  # GET: /reqs:id
+  def show
+    show_data()
+  end
+
+  # POST: /reqs
+  def create
+    show_data()
+  end
+
+  # PUT: /reqs/:id
+  def update
+    show_data()
+  end
+
+  # DELETE: /reqs/:id
+  def destroy
+    show_data()
+  end
+
+
+  private
+
+  def show_data
+    r = request
+    body = r.body.read
+    dat = {
+      method: r.request_method,
+      path: r.path_info,
+      query: r.query_string,
+      body: body,
+      params: params
+    }
+    render :json => dat
+  end
+end
