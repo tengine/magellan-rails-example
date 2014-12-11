bd7afe420e8f3a2dd5931088a2776abe216ee0cb
Yuki Noguchi <otonakaoru@gmail.com>
2014-12-11 19:58:14 +0900

    確認方法をモックで行なう方法から指定した値が保存されているか確認するよう変更

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -11,13 +11,10 @@ end
 
 describe "BooksSubscriber", :type => :request do
   describe "PUBLISH /books" do
-    before do
-      book_double = double('book')
-      expect(Book).to receive(:new).with({"title" => "sample book"}).once.and_return(book_double)
-      expect(book_double).to receive(:save).once
-    end
     it "works! (now write some real specs)" do
       publish "books", {"title" => "sample book"}.to_json
+      expect(Book.count).to eq 1
+      expect(Book.first.title).to eq 'sample book'
     end
   end
 end
