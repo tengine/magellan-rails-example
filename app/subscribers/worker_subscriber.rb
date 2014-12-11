e974563815800875b8a826b74f7043348d88508d
Shinichirow KAMITO <shinichirow@kamito.net>
2014-12-12 01:33:51 +0900

    `Magellan::Publisher.publish`を直接使うように修正。

--- app/subscribers/worker_subscriber.rb
+++ app/subscribers/worker_subscriber.rb
@@ -2,7 +2,7 @@ class WorkerSubscriber < Magellan::Subscriber::Base
 
   def pub
     logger.info("WorkerSubscriber#pub_test")
-    Magellan::Publisher.publisher.publish("rooms/0001", "#{body} from Worker.")
+    Magellan::Publisher.publish("rooms/0001", "#{body} from Worker.")
   end
 
 end
