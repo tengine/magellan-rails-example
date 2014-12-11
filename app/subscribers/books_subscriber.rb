a2ad68feaa7fbe8fd08dfb515afb8950c81f6ff3
Yuki Noguchi <otonakaoru@gmail.com>
2014-12-11 20:10:10 +0900

    MQTTを使ったCloudSQL上のデータの削除と更新に対応する実装

--- app/subscribers/books_subscriber.rb
+++ app/subscribers/books_subscriber.rb
@@ -9,4 +9,19 @@ class BooksSubscriber < Magellan::Subscriber::Base
     @book.save
   end
 
+  # /books/+/update
+  def update
+    topic =~ /books\/(.*)\/update/
+    id = $1
+
+    book = Book.find(id)
+    book_params = JSON.parse(body)
+    book_params['id'] = id
+    book.update(book_params)
+  end
+
+  # /books/delete
+  def delete_all
+    Book.delete_all
+  end
 end
