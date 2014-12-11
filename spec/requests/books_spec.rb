17c454cf9787ca5de68616ac497cc74a2bf17e8e
Yuki Noguchi <otonakaoru@gmail.com>
2014-12-11 20:08:01 +0900

    実行系の機能テストシナリオで使用するMQTTを使ったCloudSQL上のデータを更新するspecを作成

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -17,4 +17,16 @@ describe "BooksSubscriber", :type => :request do
       expect(Book.first.title).to eq 'sample book'
     end
   end
+
+  describe "PUBLISH /books/1/update" do
+    before do
+      Book.new({id: 1, title: 'sample book ver1'}).save
+    end
+
+    it "works! (now write some real specs)" do
+      publish "books/1/update", {"title" => "sample book ver2"}.to_json
+      expect(Book.count).to eq 1
+      expect(Book.first.title).to eq 'sample book ver2'
+    end
+  end
 end
