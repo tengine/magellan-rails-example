709fc7f44ddc90d1fc6330453998caf34d07b2f7
Yuki Noguchi <otonakaoru@gmail.com>
2014-12-12 15:18:53 +0900

    ワーカーが受け付けるMQTTのトピック名の仕様にあわせてspecを変更

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -10,34 +10,34 @@ RSpec.describe "Books", :type => :request do
 end
 
 describe "BooksSubscriber", :type => :request do
-  describe "PUBLISH /books" do
+  describe "PUBLISH worker/books" do
     it "works! (now write some real specs)" do
-      publish "/books", {"title" => "sample book"}.to_json
+      publish "worker/books", {"title" => "sample book"}.to_json
       expect(Book.count).to eq 1
       expect(Book.first.title).to eq 'sample book'
     end
   end
 
-  describe "PUBLISH /books/1/update" do
+  describe "PUBLISH worker/books/1/update" do
     before do
       Book.new({id: 1, title: 'sample book ver1'}).save
     end
 
     it "works! (now write some real specs)" do
-      publish "/books/1/update", {"title" => "sample book ver2"}.to_json
+      publish "worker/books/1/update", {"title" => "sample book ver2"}.to_json
       expect(Book.count).to eq 1
       expect(Book.first.title).to eq 'sample book ver2'
     end
   end
 
-  describe "PUBLISH /books/delete" do
+  describe "PUBLISH worker/books/delete" do
     before do
       Book.new({title: 'sample book1'}).save
       Book.new({title: 'sample book2'}).save
     end
 
     it "works! (now write some real specs)" do
-      publish "/books/delete", ''
+      publish "worker/books/delete", ''
       expect(Book.count).to eq 0
     end
   end
