ec4790bfcd669ab8bd4bc6e124c236d5c8bbb4b6
akima <akm2000@gmail.com>
2014-12-18 15:15:50 +0900

    get old examples back

--- spec/controllers/healthcheck_controller_spec.rb
+++ spec/controllers/healthcheck_controller_spec.rb
@@ -7,12 +7,24 @@ RSpec.describe HealthcheckController, :type => :controller do
       get :hello
       expect(response).to be_success
     end
+
+    it 'get hello' do
+      get :hello
+      json = JSON.parse(response.body)
+      expect(json['hello']).to eq('world')
+    end
   end
 
   describe "GET echo" do
     it "returns http success" do
       get :echo
       expect(response).to be_success
+    end
+
+    it 'get echo' do
+      get :echo, st: 'back'
+      json = JSON.parse(response.body)
+      expect(json['echo']).to eq('back')
     end
   end
 
