653dda852e5591187bd3123d7b7ef4a0279fa484
Chikanaga Tomoyuki <t-chikanaga@groovenauts.jp>
2015-09-01 13:05:49 +0900

    remove subscribres

--- app/subscribers/worker_subscriber.rb
+++
@@ -1,8 +0,0 @@
-class WorkerSubscriber < Magellan::Subscriber::Base
-
-  def pub
-    logger.info("WorkerSubscriber#pub_test")
-    Magellan::Publisher.publish("rooms/0001", "#{body} from Worker.")
-  end
-
-end
